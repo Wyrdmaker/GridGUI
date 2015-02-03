d17c26e86ad6eed712c0f3d1524ba5cdab7deeae
Wrymm <guillaume.hocquet@live.fr>
2015-02-03 19:17:23 +0100

    finish

--- Joujou/Joujou1.scala
+++ Joujou/Joujou1.scala
@@ -1,8 +1,10 @@
 import scala.swing._
 import scala.swing.event._
 
-class CaseLabel (p : UI, n : Int, v : String) extends Label {
+class CaseLabel (t : UI, n : Int) extends Label {
 	private var discovered = false
+	var v = "?"
+	var num = n
         listenTo(mouse.moves, mouse.clicks)
         reactions += {
                 case e : MouseEntered =>
@@ -18,30 +20,47 @@ class CaseLabel (p : UI, n : Int, v : String) extends Label {
 		if (!discovered) {
 			border = Swing.LineBorder(new Color(0,0,255))
                 	discovered = true
-			p.add()
+			t.add()
+			if (v == "?")
+				t.init(n)
 			text = v
 			v match {
-				case "b" => p.lose()
-				case " " => p.spread(n)
+				case "b" => t.lose()
+				case "0" => t.spread(n)
 				case _   => ()
 			}
 		}
 	}
+	def clear() = {
+		text = ""
+		discovered = false
+		border = Swing.LineBorder(new Color(0,0,0))
+	}
 }
 
-class GrilleMode(x : Int = 0, y : Int = 0, b : Int = 0) extends MenuItem(""){
-	action = new Action("Grille " + x + "x" + y + " (" + b + ")") {
-		def apply {
-			println(text)
+class GrilleMode(t : UI, x : Int = 0, y : Int = 0, b : Int = 0) extends MenuItem(""){
+	def f = {
+		t.contents = new GridPanel(y,x){
+                	t.lstLabel = 0 until (x * y) map (n => new CaseLabel(t, n){
+                        	border = Swing.LineBorder(new Color(0,0,0))
+                        })
+                	contents ++= t.lstLabel
+
 		}
+		t.x = x
+	        t.y = y
+        	t.nbDiscovered = 0
+        	t.nbBombs = b
 	}
+	action = Action("Grille " + x + "x" + y + " (" + b + ")")(f)
 	if (b == 0) {
-		action = new Action("Restart") {
-                	def apply {
-                        	println(text)
-                	}
-       		}
-	}
+                action = new Action("Restart") {
+                        def apply {
+                                println(text)
+				t.lstLabel.foreach(l => l.clear())
+			}
+                }
+        }
 }
 
 class UI extends MainFrame {
@@ -51,29 +70,24 @@ class UI extends MainFrame {
 	var nbDiscovered = 0
 	var nbBombs = 0
 	var lstLabel : IndexedSeq[CaseLabel] = IndexedSeq()
-	title = "GUI Program #1"
-	preferredSize = new Dimension(340, 240)
+	title = "Démineur"
+	preferredSize = new Dimension(300,300)
+	contents = new Label("Welcome ! ;)")
 	menuBar = new MenuBar {
                 contents += new Menu("Game") {
-                        contents += new GrilleMode(9,9,10)
-			contents += new GrilleMode(16,16,40)
-			contents += new GrilleMode(16,16,99)
-			contents += new GrilleMode()
+                        contents += new GrilleMode(t, 9,9,10)
+			contents += new GrilleMode(t, 16,16,40)
+			contents += new GrilleMode(t, 16,16,99)
+			contents += new GrilleMode(t)
                 }
 		contents += new Menu("About") {
                         contents += new MenuItem(new Action("Prout") {
                                 def apply {
-                                        println("New Game")
+                                        println("About what ??")
                                 }
                         })
 		}
         }
-	contents = new GridPanel(y,x){
-		lstLabel = 0 until (x * y) map (n => new CaseLabel(t, n, " "){
-			border = Swing.LineBorder(new Color(0,0,0))
-		})
-		contents ++= lstLabel
-	}
 	def add() = {
 		nbDiscovered += 1
 		if (nbDiscovered + nbBombs == x * y)
@@ -98,11 +112,30 @@ class UI extends MainFrame {
 		var lst = neighbour(n)
 		lst.foreach(n => lstLabel(n).discoverMe())
 	}
+	def init(k : Int) = {
+		var b = nbBombs
+		var r = scala.util.Random
+		neighbour(k).foreach(n => lstLabel(n).v = "#")
+		while (b > 0) {
+			var j = r.nextInt(x * y)
+			if (lstLabel(j).v == "?") {
+				lstLabel(j).v = "b"
+				b -= 1
+			}
+		}
+		lstLabel.foreach(l =>
+			if (l.v != "b") {
+				var v = 0
+				neighbour(l.num).foreach(n => if (lstLabel(n).v == "b") v += 1)
+				l.v = v.toString
+			}
+		)
+	}
 	def lose() = {
-
+		println("lose")
 	}
 	def win() = {
-
+		println("win")
 	}
 }
 
